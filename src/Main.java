684f6cc056aa47af5ec6aeff84125daa1cafdcbf
Extraneouswadd <ethanc197@nycstudents.net>
2023-12-15 10:52:51 -0500

    changed second mprint statement

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String [] args){
 
         System.out.println("Our first repo");
-        System.out.println("theworld");
-
+        System.out.println("fortnite is cool");
+        System.out.println("third");
     }
 }
